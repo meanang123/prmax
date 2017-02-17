1c5cb7874d3280e16d89f60ffac3168afd0cc1f1
Stamatia Vatsi <stamatia.vatsi@prmax.co.uk>
2017-02-17 09:57:57 +0000

    Changed paths to config files

--- prcommon/run-report-test-prcommon.bat
+++ prcommon/run-report-test-prcommon.bat
@@ -1,3 +1,3 @@
 call \Projects\tg15env\Scripts\activate.bat
-SET PYTHONPATH=\Projects\prmaxtest\prcommon;\Projects\ttl
+SET PYTHONPATH=\Projects\prmax\test\prcommon;\Projects\prmax\test\ttl
 \Projects\tg15env\Scripts\python prcommon\utilities\programs\services\reportbuilder.py
