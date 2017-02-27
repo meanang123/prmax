1a62dd471c614ee40398cd1a328e2d8accbd1cf5
Stamatia Vatsi <stamatia.vatsi@prmax.co.uk>
2017-02-27 09:52:58 +0000

    Python path corrected

--- prmax/start-prmax-live.bat
+++ prmax/start-prmax-live.bat
@@ -1,3 +1,3 @@
 call \Projects\tg15env\Scripts\activate.bat
-SET PYTHONPATH=c:\projects\live\ttl;c:\projects\prmax\live\prmax;c:\projects\prmax\live\prcommon
+SET PYTHONPATH=c:\projects\prmax\live\ttl;c:\projects\prmax\live\prmax;c:\projects\prmax\live\prcommon
 \Projects\tg15env\Scripts\python start-prmax.py dev-15.cfg
