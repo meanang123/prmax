73f1a93ad3022f5a1183056c42d8fb7bb483e2e0
ChrisHoy <chris.hoy@prmax.co.uk>
2017-04-13 11:28:42 +0100

    Fixed : Clipping Import Fixed : blueboo missing icons

--- prmax/prmax/setup/sql/temp_tables.sql
+++ prmax/prmax/setup/sql/temp_tables.sql
@@ -1,4 +1,7 @@
-﻿ALTER TABLE userdata.clippings
-	DROP CONSTRAINT clippings_outletid_fkey;
-ALTER TABLE userdata.clippings
-	ADD CONSTRAINT clippings_outletid_fkey1 FOREIGN KEY (outletid) REFERENCES outlets (outletid) ON UPDATE NO ACTION ON DELETE SET NULL;
+﻿ALTER TABLE userdata.clippings DROP CONSTRAINT clippings_outletid_fkey;
+ALTER TABLE userdata.clippings ADD CONSTRAINT clippings_outletid_fkey1 FOREIGN KEY (outletid) REFERENCES outlets (outletid) ON UPDATE NO ACTION ON DELETE SET NULL;
+
+update outlets set prmax_outlettypeid = 13 WHERE prmax_outlettypeid = 68;
+update outlets set prmax_outlettypeid = 15 WHERE prmax_outlettypeid = 67;
+DELETE FROM internal.prmax_outlettypes WHERE prmax_outlettypeid IN (67,68);
+
