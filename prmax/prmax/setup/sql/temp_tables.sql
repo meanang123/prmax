844d90a6227c107311b99151d5c1f3096cf6106f
Stamatia Vatsi <stamatia.vatsi@prmax.co.uk>
2017-06-19 09:33:20 +0100

    typo error

--- prmax/prmax/setup/sql/temp_tables.sql
+++ prmax/prmax/setup/sql/temp_tables.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿ALTER TABLE userdata.clippings DROP CONSTRAINT clippings_outletid_fkey;
 ALTER TABLE userdata.clippings ADD CONSTRAINT clippings_outletid_fkey1 FOREIGN KEY (outletid) REFERENCES outlets (outletid) ON UPDATE NO ACTION ON DELETE SET NULL;
 
