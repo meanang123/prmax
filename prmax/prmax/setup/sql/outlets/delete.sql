13737e6a9f01ff4ede3122ee0290784f305cbfc7
Chris Hoy <chris.hoy@paperround.net>
2017-10-31 15:37:57 +0000

    Changed : all key links for clippings tables import delete performance

--- prmax/prmax/setup/sql/outlets/delete.sql
+++ prmax/prmax/setup/sql/outlets/delete.sql
@@ -27,3 +27,29 @@ DELETE FROM outlets WHERE outletid = p_outletid;
 END;
 
 $$ LANGUAGE plpgsql SECURITY DEFINER;
+
+CREATE OR REPLACE FUNCTION outlet_delete_pre_clean( p_outletid integer)
+RETURNS void AS $$
+
+BEGIN
+-- remove from all lists
+DELETE FROM searchsession WHERE outletid = p_outletid;
+
+-- Remove links from distributions
+UPDATE userdata.listmembers SET outletid = NULL,employeeid = NULL
+	FROM userdata.list AS l WHERE l.listid = userdata.listmembers.listid AND listtypeid = 2 AND userdata.listmembers.outletid = p_outletid AND l.fixed = true ;
+
+-- Remove record from standard lists
+DELETE userdata.listmembers
+	FROM userdata.list AS l WHERE l.listid = userdata.listmembers.listid AND listtypeid = 1 AND userdata.listmembers.outletid = p_outletid;
+
+-- cleanup clippings
+UPDATE userdata.clippings SET  outletid = NULL, listmemberid = NULL WHERE outletid = p_outletid;
+UPDATE clippingstore SET outletid = NULL, employeeid = NULL WHERE outletid = p_outletid;
+
+DELETE research.bounceddistribution WHERE outletid = p_outletid;
+DELETE research.researchprojectitem WHERE outletid = p_outletid;
+
+END;
+
+$$ LANGUAGE plpgsql SECURITY DEFINER;
