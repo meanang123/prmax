d0e92f5f31e7218afec63712fab5882f0ae4c08a
Chris Hoy <chris.hoy@paperround.net>
2018-01-19 16:13:30 +0000

    Change : sql temp

--- prmax/prmax/setup/sql/temp_tables.sql
+++ prmax/prmax/setup/sql/temp_tables.sql
@@ -1,26 +1,4 @@
-﻿
-
--- reset index's for clipping speed up delete ?
-ALTER TABLE userdata.clippings DROP CONSTRAINT clippings_listmemberid_fkey;
-ALTER TABLE userdata.clippings DROP CONSTRAINT clippings_outletid_fkey1;
-ALTER TABLE clippingstore DROP CONSTRAINT clippingstore_outletid_fkey;
-
-
-
-ALTER TABLE userdata.clippings ADD CONSTRAINT fk_listmembers FOREIGN KEY (listmemberid) REFERENCES userdata.listmembers (listmemberid)
-   ON UPDATE NO ACTION ON DELETE SET NULL;
-CREATE INDEX fki_listmembers ON userdata.clippings(listmemberid);
-
-ALTER TABLE userdata.clippings ADD CONSTRAINT fk_cl_outletid FOREIGN KEY (outletid) REFERENCES outlets (outletid)
-   ON UPDATE NO ACTION ON DELETE SET NULL;
-CREATE INDEX fki_cl_outletid ON userdata.clippings(outletid);
-
-ALTER TABLE clippingstore ADD CONSTRAINT fk_cs_outletid FOREIGN KEY (outletid) REFERENCES outlets (outletid)
-   ON UPDATE NO ACTION ON DELETE SET NULL;
-CREATE INDEX fki_cs_outletid ON clippingstore(outletid);
-
-
-INSERT INTO internal.reporttemplates VALUES (32, -1, 'Statistics Report', '<queries><query type="CUSTOM"></query></queries>', '', 9, 'StatisticsReport');
+﻿INSERT INTO internal.reporttemplates VALUES (32, -1, 'Statistics Report', '<queries><query type="CUSTOM"></query></queries>', '', 9, 'StatisticsReport');
 
 UPDATE communications SET instagram = '' WHERE instagram is null;
 UPDATE userdata.client SET instagram = '' WHERE instagram is null;
@@ -31,6 +9,7 @@ ALTER TABLE internal.sortorder ALTER sortorderfieldname TYPE character varying (
 UPDATE internal.sortorder
    SET sortorderfieldname = 'UPPER(familyname) %order%, UPPER(firstname) %order%'
  WHERE sortorderid = 2;
+
 
 DROP VIEW search_results_view_standard;
 CREATE OR REPLACE VIEW search_results_view_standard AS
