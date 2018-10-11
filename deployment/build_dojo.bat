f61f3d7213b2ae4fe52b57e29ac53429ab8f39bd
Chris <tethystec@gmail.com>
2018-10-11 14:39:39 +0100

    Fixed : bat files

--- deployment/build_dojo.bat
+++ deployment/build_dojo.bat
@@ -1,2 +1,2 @@
 cd \Projects\prmax\development\deployment\dojo\util\buildscripts
-java -Xms1024m -Xmx1024m  -cp ../shrinksafe/js.jar;../closureCompiler/compiler.jar;../shrinksafe/shrinksafe.jar org.mozilla.javascript.tools.shell.Main  ../../dojo/dojo.js baseUrl=../../dojo load=build profile=%1 action=clean,release copyTests=false stripConsole=all
+java -Xmx1024m  -cp ../shrinksafe/js.jar;../closureCompiler/compiler.jar;../shrinksafe/shrinksafe.jar org.mozilla.javascript.tools.shell.Main  ../../dojo/dojo.js baseUrl=../../dojo load=build profile=%1 action=clean,release copyTests=false stripConsole=all
--- prmax/buildrelease/build_dojo_test.bat
+++ prmax/buildrelease/build_dojo_test.bat
@@ -1,2 +1,2 @@
 cd \Projects\prmax\development\prmax\buildrelease\dojo\util\buildscripts
-java -Xmx2048m -classpath ../shrinksafe/js.jar;../shrinksafe/shrinksafe.jar org.mozilla.javascript.tools.shell.Main build.js profile=prmax releaseName=prmax action=clean,release copyTests=false version=%1 optimize=shrinksafe cssOptimize=comments  stripConsole=all
+java -Xmx1024m -classpath ../shrinksafe/js.jar;../shrinksafe/shrinksafe.jar org.mozilla.javascript.tools.shell.Main build.js profile=prmax releaseName=prmax action=clean,release copyTests=false version=%1 optimize=shrinksafe cssOptimize=comments  stripConsole=all
