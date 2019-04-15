0d84d746b0ee0075f75fc467e01e9201fc01d611
Chris <tethystec@gmail.com>
2019-04-15 11:43:52 +0100

    Changed : added index

--- prmax/prmax/setup/sql/temp_tables.sql
+++ prmax/prmax/setup/sql/temp_tables.sql
@@ -1,18 +1,18 @@
 ﻿/*
 --Direct Debit and monthly payments new expiry year
-UPDATE internal.customers 
+UPDATE internal.customers
 SET licence_expire = licence_expire + interval '15 years'
 WHERE customerstatusid = 2
 AND paymentmethodid in (2,3)
 AND licence_expire between '2020-01-01' and '2020-12-31'
 
-UPDATE internal.customers 
+UPDATE internal.customers
 SET advance_licence_expired = advance_licence_expired + interval '15 years'
 WHERE customerstatusid = 2
 AND paymentmethodid in (2,3)
 AND advance_licence_expired between '2020-01-01' and '2020-12-31'
 
-UPDATE internal.customers 
+UPDATE internal.customers
 SET updatum_end_date = updatum_end_date + interval '15 years'
 WHERE customerstatusid = 2
 AND paymentmethodid in (2,3)
@@ -21,3 +21,6 @@ AND updatum_end_date between '2020-01-01' and '2020-12-31'
 
 
 UPDATE internal.outletsearchtypes SET outletsearchtypename = 'B2B Magazines' WHERE outletsearchtypeid = 4;
+
+-- missing index
+CREATE INDEX ON userdata.clippings (customerid ASC NULLS LAST, created_date ASC NULLS LAST);
